5e3f7c52984749c8633b9f4702f9de231ba186d7
Qihuang Chen <cqh99@uw.edu>
2023-02-07 21:56:31 -0800

    go sql practice

---
+++ complete/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const createTable string = `CREATE TABLE courses(
+	department TEXT,
+  	code INT,
+  	description TEXT
+);`
+
+const insertTuple string = `insert into courses (department, code, description) VALUES (?,?,?);`
+
+const getCseCourses string = `select department, code, description from courses `
+
+func main() {
+	// create .db file
+	database, err := sql.Open("sqlite3", "./ucsd.db")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// create table in .db file
+	statement, err := database.Prepare(createTable)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	statement.Exec()
+
+	// insert tuples into table 'courses'
+	// notice ? in the insert statement.
+	statement, _ = database.Prepare(insertTuple)
+	statement.Exec("cse", 100, "data structure")
+	statement.Exec("cse", 224, "network systems")
+	statement.Exec("cse", 221, "operating systems")
+	statement.Exec("cse", 232, "database")
+	statement.Exec("cse", 202, "algorithms")
+	statement.Exec("cse", 240, "computer architecture")
+	statement.Exec("ece", 225, "Probability")
+	statement.Exec("ece", 260, "VLSI")
+
+	// retrieve all cse courses tuples in ascending order by code.
+	rows, err := database.Query(getCseCourses)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	var department string
+	var code int
+	var description string
+	for rows.Next() {
+		rows.Scan(&department, &code, &description)
+		fmt.Println(department + strconv.Itoa(code) + ": " + description)
+	}
+
+}
